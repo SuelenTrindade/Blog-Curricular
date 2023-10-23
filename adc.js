afa42fc695cad78acb8e441bab8b602d00814618
SuelenTrindade <107163629+SuelenTrindade@users.noreply.github.com>
2023-10-23 17:04:14 -0300

    Atualização

---
+++ adc.js
@@ -0,0 +1,18 @@
+/*Insira no rodapé das três páginas que você desenvolveu na Unidade 
+2 (homepage, mais uma página e a página de formulário): nome completo, ano corrente, 
+seus endereços das redes sociais, como: twitter, facebook, instagram, LinkedIn, 
+sexo e localização de seu polo. Cada informação deve constar em linhas 
+separadas usando VARIÁVEIS para o armazenamento das informações 
+pessoais e exibindo-as com o document.write(), por meio da linguagem de script: JavaScript.*/
+
+var nome = "Suelen Stefanni da Silva Trindade"
+var ano = 2023
+var redeSocial = "@SuelenTrindade"
+var sexo = "Feminino"
+var local = "Araraquara/SP"
+
+document.write(nome);
+document.write(<p>ano</p>)
+document.write(<p>redeSocial</p>>)
+document.write(<p>sexo</p>)
+document.write(<p>local</p>)
---
+++ js/adc.js
@@ -0,0 +1,16 @@
+/*Insira no rodapé das três páginas que você desenvolveu na Unidade 
+2 (homepage, mais uma página e a página de formulário): nome completo, ano corrente, 
+seus endereços das redes sociais, como: twitter, facebook, instagram, LinkedIn, 
+sexo e localização de seu polo. Cada informação deve constar em linhas 
+separadas usando VARIÁVEIS para o armazenamento das informações 
+pessoais e exibindo-as com o document.write(), por meio da linguagem de script: JavaScript.*/
+var nome = "Suelen Stefanni da Silva Trindade"
+document.write(nome, "<br>");
+var ano = "Ano da Edição 2023"
+document.write(ano,"<br>");
+var redeSocial = "Instagram: @SuelenTrindade"
+document.write(redeSocial,"<br>");
+var sexo ="Sexo: Feminino" 
+document.write(sexo, "<br>");
+var local = "Polo: Araraquara/SP"
+document.write(local, "<br>");
